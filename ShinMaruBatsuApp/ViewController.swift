f27f4033b5a91cb3bcbb47e42cd38e670be782ae
ryo-ksb <ksb12yota@gmail.com>
2018-10-28 23:56:07 +0900

    Initial Commit

---
+++ ShinMaruBatsuApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShinMaruBatsuApp
+//
+//  Created by 小芝亮太 on 2018/10/28.
+//  Copyright © 2018 小芝亮太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
