c518beafe1b1bc2cc37577699d8bcd21798a0319
ryo-ksb <ksb12yota@gmail.com>
2018-10-31 10:47:34 +0900

    第2週課題1終了

--- ShinMaruBatsuApp/ViewController.swift
+++ ShinMaruBatsuApp/ViewController.swift
@@ -9,12 +9,106 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentQuestionNum: Int = 0
+    
+    var queAnsRe: [[String: Any]] = []
+    
+    @IBOutlet weak var questionTextField: UITextView!
+    
+    @IBAction func toButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if UserDefaults.standard.object(forKey: "queAns") == nil {
+            UserDefaults.standard.set([], forKey: "queAns")
+        }
+        
+        showQuestion()
+        
     }
-
+    
+    /// Storyboadでunwind sequeを引くために必要
+    @IBAction func unwindToFirstView(segue: UIStoryboardSegue) {
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+ 
+        showQuestion()
+        
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+        alert.addAction(close)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showQuestion() {
+        
+        queAnsRe = UserDefaults.standard.object(forKey: "queAns") as! [[String : Any]]
+        
+        if queAnsRe.count > currentQuestionNum {
+        
+            let queAnsDic = queAnsRe[currentQuestionNum]
+        
+            if let que = queAnsDic["question"] as? String {
+           
+                questionTextField.text = que
+                
+            }
+            
+        } else {
+            
+                questionTextField.text = "問題がありません、問題を作りましょう！"
+            
+        }
+    }
+    
+    func checkAnswer(yourAnswer: Bool) {
+        
+        queAnsRe = UserDefaults.standard.object(forKey: "queAns") as! [[String : Any]]
+        
+        if queAnsRe.count > currentQuestionNum {
+        
+            let queAnsDic = queAnsRe[currentQuestionNum]
+        
+            if let ans = queAnsDic["answer"] as? Bool {
+            
+                if yourAnswer == ans {
+                
+                    currentQuestionNum += 1
+                
+                    showAlert(message: "正解")
+                
+                } else {
+                
+                    showAlert(message: "不正解")
+                
+                }
+            }
+        
+            if currentQuestionNum >= queAnsRe.count {
+                currentQuestionNum = 0
+            }
+        
+            showQuestion()
+        
+        }
+    }
+    
+    @IBAction func MaruButton(_ sender: Any) {
+        checkAnswer(yourAnswer: true)
+    }
+    
+    @IBAction func BatsuButton(_ sender: Any) {
+        checkAnswer(yourAnswer: false)
+    }
+    
 
 }
 
---
+++ ShinMaruBatsuApp/addQuestionViewController.swift
@@ -0,0 +1,97 @@
+//
+//  addQuestionViewController.swift
+//  ShinMaruBatsuApp
+//
+//  Created by 小芝亮太 on 2018/10/29.
+//  Copyright © 2018 小芝亮太. All rights reserved.
+//
+
+import UIKit
+
+class addQuestionViewController: UIViewController {
+    
+    var questions: String!
+    
+    var answers: Bool!
+    
+    var queAns: [[String: Any]] = []
+    
+    @IBOutlet weak var questionLabel: UITextField!
+    
+    @IBAction func backButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBOutlet weak var MaruBatsu: UISegmentedControl!
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+        alert.addAction(close)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func saveQuestion(_ sender: Any) {
+        
+        if MaruBatsu.selectedSegmentIndex == 0 {
+            answers = false
+        } else {
+            answers = true
+        }
+        
+        if questionLabel.text != "" {
+            
+            questions = questionLabel.text
+        
+            queAns = UserDefaults.standard.object(forKey: "queAns") as! [[String : Any]]
+        
+            queAns.append([
+            
+                "question": questions,
+                "answer": answers
+                
+            ])
+        
+            UserDefaults.standard.set(queAns, forKey: "queAns")
+            
+        } else {
+         
+            showAlert(message: "問題文を入力してください")
+        
+        }
+        
+        questionLabel.text = ""
+        
+        print("\(queAns)")
+        
+    }
+     
+    
+    @IBAction func deleteQuestions(_ sender: Any) {
+        
+        UserDefaults.standard.removeObject(forKey: "queAns")
+        
+        UserDefaults.standard.set([], forKey: "queAns")
+        
+        showAlert(message: "問題をすべて削除しました")
+        
+        print("\(queAns)")
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
